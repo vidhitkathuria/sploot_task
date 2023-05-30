ed653a25220d33f5ddd58e14cfc155ea7891605c
vidhitkathuria <72202024+vidhitkathuria@users.noreply.github.com>
2023-05-31 04:28:51 +0530

    env var name correction

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports.mongoConnection = mongoose.connect(
-  process.env.DB_URL,
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
